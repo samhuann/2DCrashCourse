e46ccd87b62796ce6745a2f2a468759a8c4a2c93
Samuel Huang <shuan143@jhu.edu>
2025-07-06 17:58:47 -0400

    Animation Walking and Running

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -9,13 +9,46 @@ public class PlayerController : MonoBehaviour
     public float walkSpeed = 5f;
 
     Vector2 moveInput;
-    public bool isMoving;
+
+    [SerializeField]
+    private bool _isMoving = false;
+    public bool isMoving
+    {
+        get
+        {
+            return _isMoving;
+        }
+        private set
+        {
+            _isMoving = value;
+            animator.SetBool("isMoving", value);
+        }
+    }
+    [SerializeField]
+
+    private bool _isRunning = false;
+
+
+    public bool isRunning
+    {
+        get
+        {
+            return _isRunning;
+        }
+        private set
+        {
+            _isRunning = value;
+            animator.SetBool("isRunning", value);
+        }
+    }
 
     Rigidbody2D rb;
+    Animator animator;
 
     private void Awake()
     {
-        rb = GetComponent<Rigidbody2D>();   
+        rb = GetComponent<Rigidbody2D>();
+        animator = GetComponent<Animator>();
     }
 
     void Start()
@@ -32,7 +65,7 @@ public class PlayerController : MonoBehaviour
     void FixedUpdate()
     {
         rb.linearVelocity = new Vector2(moveInput.x * walkSpeed, rb.linearVelocity.y);
-        
+
     }
 
     public void OnMove(InputAction.CallbackContext context)
@@ -40,5 +73,35 @@ public class PlayerController : MonoBehaviour
         moveInput = context.ReadValue<Vector2>();
 
         isMoving = moveInput != Vector2.zero;
+
+        SetFacingDirection(moveInput.x);
+        
+    }
+
+    private void SetFacingDirection(float x)
+    {
+        if (x > 0)
+        {
+            transform.localScale = new Vector3(1, 1, 1); // Facing right
+        }
+        else if (x < 0)
+        {
+            transform.localScale = new Vector3(-1, 1, 1); // Facing left
+        }
+
+    }
+
+    public void OnRun(InputAction.CallbackContext context)
+    {
+        if (context.started)
+        {
+            isRunning = true;
+            walkSpeed *= 2; // Double the speed when running
+        }
+        else if (context.canceled)
+        {
+            isRunning = false;
+            walkSpeed /= 2; // Reset to normal speed when not running
+        }
     }
 }
